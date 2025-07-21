64e7fc1d77bbee138bbde1b522d7b757900a9eae
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-21 17:49:07 +0000

    coloque essa imagem na avliação 1

--- src/components/landing/reviews.tsx
+++ src/components/landing/reviews.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Star, StarHalf } from 'lucide-react';
 
 const reviews = [
-  { name: 'Pedro Souza', image: 'https://placehold.co/100x100', rating: 5, comment: 'meu deus, é um vídeo melhor que o outro', hint: 'man portrait'},
+  { name: 'Pedro Souza', image: '/__tmp__/image.jpeg', rating: 5, comment: 'meu deus, é um vídeo melhor que o outro', hint: 'man portrait'},
   { name: 'Carlos Álvez', image: 'https://placehold.co/100x100', rating: 5, comment: 'se é loco, nunca mais Xvideos..', hint: 'man portrait'},
   { name: 'Lucas Rodrigues', image: 'https://placehold.co/100x100', rating: 4, comment: 'Essa melody é muito safada pqp, melhor grupo que ja entrei', hint: 'man smiling'},
   { name: 'Marcos Ferreira', image: 'https://placehold.co/100x100', rating: 5, comment: 'Muito barato pela qualidade que entrega, chegou muito rápido e as mina são top!', hint: 'man glasses'},
