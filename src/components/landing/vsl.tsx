5bcb8550065fa4315c7113fc7b97f5fcc961e8b0
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-21 20:45:19 +0000

    show, agora aumente um pouco a altura da vsl

--- src/components/landing/vsl.tsx
+++ src/components/landing/vsl.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Vsl() {
   return (
     <section className="w-full bg-black flex justify-center items-center">
-      <div className="w-full aspect-video relative" style={{ maxHeight: '250px' }}>
+      <div className="w-full aspect-video relative" style={{ maxHeight: '280px' }}>
          <Image
             src="https://placehold.co/1080x1920"
             alt="VSL Placeholder"
