e73e78702f3283954dd3828e4ceda262ca3de873
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-21 21:21:53 +0000

    o "Assine já!" primeiro, antes da liberação imediata.. está piscando as

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -91,12 +91,16 @@ export default {
         'scale-pulse': {
           '0%, 100%': { transform: 'scale(1)' },
           '50%': { transform: 'scale(1.05)' },
+        },
+        'pulse': {
+          '50%': { opacity: '.5' },
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'scale-pulse': 'scale-pulse 1s infinite',
+        'pulse': 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
