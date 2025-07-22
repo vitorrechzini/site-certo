60dd9eb98420ffb3e7264a5ffcc6e430906301cc
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-22 16:42:58 +0000

    aumentou muit, diminua

--- src/components/landing/vsl.tsx
+++ src/components/landing/vsl.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 export default function Vsl() {
   return (
     <section className="w-full bg-black flex justify-center items-center">
-      <div className="w-full aspect-video relative">
+      <div className="w-full aspect-video relative max-h-[70vh]">
          <Image
             src="https://placehold.co/1080x1920"
             alt="VSL Placeholder"
             fill
-            style={{objectFit: 'cover'}}
+            style={{objectFit: 'contain'}}
             className="w-full h-full"
             data-ai-hint="video thumbnail"
           />
