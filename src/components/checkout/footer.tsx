e18820a1e7e8f8618545999af41f232328d446a0
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-22 17:24:04 +0000

    troque o "Onlyfree" em cima de todos os direitos reservados pelas logo d

--- src/components/checkout/footer.tsx
+++ src/components/checkout/footer.tsx
@@ -6,7 +6,13 @@ export default function CheckoutFooter() {
   return (
     <footer className="w-full mt-8 bg-black text-center py-8 px-4 text-white">
         <div className="flex items-center justify-center w-full mb-4">
-            <p className="font-bold">OnlyFree</p>
+            <Image
+                src="/logo.png"
+                alt="OnlyFree Logo"
+                width={150}
+                height={40}
+                style={{ objectFit: 'contain' }}
+            />
         </div>
         <p className="text-xs text-gray-400">
             Todos os Direitos Reservados 2025 - <span className="text-blue-500">OnlyFree</span>
