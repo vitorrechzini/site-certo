323ce9134de3e464abad5fcf328138409843632b
Vitor Zini <vitor.zinirech@gmail.com>
2025-07-23 18:27:12 +0000

    diminua um pouco a altura da vsl

--- src/components/landing/vsl.tsx
+++ src/components/landing/vsl.tsx
@@ -3,7 +3,7 @@
 export default function Vsl() {
   return (
     <section className="w-full bg-black flex justify-center items-center">
-      <div className="w-full aspect-video relative max-h-[70vh]">
+      <div className="w-full aspect-video relative max-h-[60vh]">
         <video
           src="https://priveflixoficial.com/wp-content/uploads/2025/03/pvvideos.webm"
           autoPlay
